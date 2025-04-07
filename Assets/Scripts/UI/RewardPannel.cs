a3a393fac194156ed727810a4c83ab48f895cddd
NhacPhi <52564362+NhacPhi@users.noreply.github.com>
2025-04-07 10:22:18 +0700

    +Add reward panel assets and create prefab for it.

---
+++ Assets/Scripts/UI/RewardPannel.cs
@@ -0,0 +1,29 @@
+using Cysharp.Threading.Tasks;
+using Observer;
+using UnityEngine;
+using VContainer;
+
+namespace UI
+{
+    public class RewardPannel : PanelBase
+    {
+        protected override void OnAwake()
+        {
+            base.OnAwake();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
+
